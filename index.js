4e66ef35cb33d4f11ab1fd82c32e7f1f6b72e688
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2022-11-08 11:58:39 +0600

    port test passed

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+const port = process.env.PORT || 5000;
+
+// Middlewares 
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection 
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('food server is running..');
+});
+
+userName: foodMartDB
+passWord: JUTyUmbwcNZhGUB1
+
+app.listen(port, () => {
+    console.log('server is running on port: ', port);
+})
