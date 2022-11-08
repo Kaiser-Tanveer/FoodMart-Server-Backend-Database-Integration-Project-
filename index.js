e551b3c7551a70dc20b51eda0d27ecd867a945a9
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2022-11-08 20:43:29 +0600

    data limit set

--- index.js
+++ index.js
@@ -1,30 +1,65 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // Middlewares 
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority";
+// MongoDB Connection 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+
+
+// All the crud operations inside this async function
+const run = async () => {
+    try {
+        // creating Services collection in MongoDB 
+        const halfCollection = client.db('foodMart').collection('home');
+        const foodCollection = client.db('foodMart').collection('services');
+
+        // Read from DB
+        app.get('/home', async (req, res) => {
+            const query = {};
+            const cursor = halfCollection.find(query);
+            const foods = await cursor.toArray();
+            res.send(foods);
+        })
+
+        // Read from DB
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = foodCollection.find(query);
+            const foods = await cursor.toArray();
+            res.send(foods);
+        })
+
+        // Read specific data by id 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await foodCollection.findOne(query);
+            console.log(result);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
     res.send('food server is running..');
 });
 
-userName: foodMartDB
-passWord: JUTyUmbwcNZhGUB1
 
 app.listen(port, () => {
     console.log('server is running on port: ', port);
